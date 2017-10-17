0fe654667e2332e87f4af50761af6b9df90b33b2
Jakub Anioła <jakub.aniola@rsqtechnologies.com>
2017-10-17 19:37:10 +0200

    initial commit

---
+++ app/src/main/java/com/example/jakubaniola/patientcard/QRReaderActivity.kt
@@ -0,0 +1,12 @@
+package com.example.jakubaniola.patientcard
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class QRReaderActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_qr__reader)
+    }
+}
