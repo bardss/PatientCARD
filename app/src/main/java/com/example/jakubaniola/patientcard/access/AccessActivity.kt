6e1cc5d6e0486b0fe52ba0d041f36c7da4434012
Jakub Anioła <jakub.aniola@rsqtechnologies.com>
2017-11-04 15:56:00 +0100

    qr reader working

--- app/src/main/java/com/example/jakubaniola/patientcard/access/AccessActivity.kt
+++ app/src/main/java/com/example/jakubaniola/patientcard/access/AccessActivity.kt
@@ -1,11 +1,13 @@
 package com.example.jakubaniola.patientcard.access
 
-import android.os.Bundle
+import android.content.Intent
 import com.example.jakubaniola.patientcard.R
 import com.example.jakubaniola.patientcard.base.BasePresenter
+import com.example.jakubaniola.patientcard.qrreader.QRReaderActivity
 import com.rsqtechnologies.rsqphysio.base.BaseActivity
 import easymvp.annotation.ActivityView
 import easymvp.annotation.Presenter
+import kotlinx.android.synthetic.main.activity_access.*
 
 @ActivityView(layout = R.layout.activity_access, presenter = AccessPresenterImpl::class)
 class AccessActivity : BaseActivity(), AccessView {
@@ -17,8 +19,18 @@ class AccessActivity : BaseActivity(), AccessView {
         return presenter
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_access)
+    override fun onStart() {
+        super.onStart()
+        setupLoginButton()
+    }
+
+    private fun setupLoginButton() {
+        login_button.setOnClickListener { openQRReaderActivity() }
+    }
+
+    private fun openQRReaderActivity() {
+        startActivity(Intent(this, QRReaderActivity::class.java))
+        overridePendingTransition(R.anim.fade_in, R.anim.fade_out)
+        finish()
     }
 }
--- app/src/main/java/com/example/jakubaniola/patientcard/qrreader/QRReaderActivity.kt
+++ app/src/main/java/com/example/jakubaniola/patientcard/qrreader/QRReaderActivity.kt
@@ -1,17 +1,26 @@
 package com.example.jakubaniola.patientcard.qrreader
 
-import android.os.Bundle
 import com.example.jakubaniola.patientcard.R
 import com.example.jakubaniola.patientcard.base.BasePresenter
 import com.example.jakubaniola.patientcard.splash.QRReaderPresenter
 import com.example.jakubaniola.patientcard.splash.QRReaderPresenterImpl
 import com.example.jakubaniola.patientcard.splash.QRReaderView
 import com.rsqtechnologies.rsqphysio.base.BaseActivity
+import com.yanzhenjie.permission.AndPermission
+import com.yanzhenjie.permission.Permission
+import com.yanzhenjie.permission.PermissionListener
 import easymvp.annotation.ActivityView
 import easymvp.annotation.Presenter
+import github.nisrulz.qreader.QRDataListener
+import github.nisrulz.qreader.QREader
+import kotlinx.android.synthetic.main.activity_qr_reader.*
+import timber.log.Timber
 
-@ActivityView(layout = R.layout.activity_qr__reader, presenter = QRReaderPresenterImpl::class)
+
+@ActivityView(layout = R.layout.activity_qr_reader, presenter = QRReaderPresenterImpl::class)
 class QRReaderActivity : BaseActivity(), QRReaderView {
+
+    private var qrReader : QREader? = null
 
     @Presenter
     lateinit var presenter: QRReaderPresenter
@@ -20,8 +29,50 @@ class QRReaderActivity : BaseActivity(), QRReaderView {
         return presenter
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_qr__reader)
+    override fun onStart() {
+        super.onStart()
+        checkPermission()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (qrReader != null) {
+            qrReader!!.initAndStart(qr_reader_surface_view)
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        if (qrReader != null) {
+            qrReader!!.releaseAndCleanup()
+        }
+    }
+
+    private fun setupQRReader() {
+        qrReader = QREader.Builder(this, qr_reader_surface_view, QRDataListener { data ->
+            Timber.e("QREader", "Value : " + data)
+        }).facing(QREader.BACK_CAM)
+                .enableAutofocus(true)
+                .height(qr_reader_surface_view!!.height)
+                .width(qr_reader_surface_view.width)
+                .build()
+        qrReader!!.initAndStart(qr_reader_surface_view)
+    }
+
+    private fun checkPermission(){
+        AndPermission
+                .with(this)
+                .requestCode(0)
+                .permission(Permission.CAMERA)
+                .callback(object : PermissionListener {
+                    override fun onSucceed(requestCode: Int, grantPermissions: List<String>) {
+                        setupQRReader()
+                    }
+
+                    override fun onFailed(requestCode: Int, deniedPermissions: List<String>) {
+                    }
+                })
+                .start()
+
     }
 }
